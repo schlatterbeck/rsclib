1798342b1a924c49d7f47aa8418944fcfb12f915
Ralf Schlatterbeck <rsc@runtux.com>
2007-10-23 17:35:09 +0000

    Added grepmime

---
+++ rsclib/grepmime.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python2.4
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2007 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Reichergasse 131, A-3411 Weidling.
+# Web: http://www.runtux.com Email: office@runtux.com
+# All rights reserved
+# ****************************************************************************
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+
+import re
+from   email               import message_from_file
+from   rsclib.autosuper    import autosuper
+
+class Mail (autosuper) :
+    def __init__ (self, fd) :
+        self.msg    = message_from_file (fd)
+    # end def __init__
+
+    def grep (self, pattern) :
+        pattern = re.compile (pattern)
+        found = []
+        for part in self.msg.walk () :
+            if part.get_content_maintype () == 'text' :
+                payload = part.get_payload (decode = 1)
+                for line in payload.split ('\n') :
+                    if pattern.search (line) :
+                        found.append (line)
+        return found
+    # end def grep
+# end class Mail
+
+if __name__ == "__main__" :
+    import sys
+    sys.exit (bool (Mail (sys.stdin).grep ('huhu')))

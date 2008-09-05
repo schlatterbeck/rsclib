c8eb28ddba19f7f87e6cb30f5bc637a10141121c
Ralf Schlatterbeck <rsc@runtux.com>
2008-09-05 09:43:13 +0000

    Binomial coefficient

--- rsclib/Math.py
+++ rsclib/Math.py
@@ -82,3 +82,16 @@ def lcm (m, n) :
     """
     return m * (n // gcd (m, n))
 # end def lcm
+
+def binom (n, m) :
+    """ Compute binomial coefficient of n, m """
+    b = [0] * (n + 1)
+    b [0] = 1
+    for i in xrange (1, n + 1) :
+        b [i] = 1
+        j = i - 1
+        while j > 0 :
+            b [j] += b [j - 1]
+            j -= 1
+    return b [m]
+# end def binom

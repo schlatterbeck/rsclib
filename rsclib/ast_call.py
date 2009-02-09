3968b6a6c7837fc646b7e283af1b72afdb61a8d3
Ralf Schlatterbeck <rsc@runtux.com>
2009-02-09 17:35:17 +0000

    First round of making this work
    ..with asterisk 1.2 and 1.4:
    Matching of action id
    Matching of unique id in response (1.4 and hopefully above)
    Matching of channel for 1.2

--- rsclib/ast_call.py
+++ rsclib/ast_call.py
@@ -35,6 +35,7 @@ class Config (Config_File) :
     def __init__ (self, config = 'autocaller', path = '/etc/autocaller') :
         self.__super.__init__ \
             ( path, config
+            , MATCH_CHANNEL         = False
             , ASTERISK_HOST         = 'localhost'
             , ASTERISK_MGR_ACCOUNT  = 'user'
             , ASTERISK_MGR_PASSWORD = 'secret'
@@ -51,17 +52,22 @@ class Config (Config_File) :
 # end class Config
 
 class Call (object) :
-    def __init__ (self, call_manager, callid) :
+    def __init__ (self, call_manager, actionid, uniqueid = None) :
         self.manager    = call_manager
-        self.callid     = callid
+        self.actionid   = actionid
+        self.uniqueid   = None
+        self.callid     = None
         self.events     = []
         self.uids       = {}
         self.causecode  = 0
         self.causetext  = ''
         self.dialstatus = ''
+        if uniqueid is not None :
+            self._set_id (uniqueid)
     # end def __init__
 
     def append (self, event) :
+        assert (self.uniqueid)
         id = event.headers ['Uniqueid']
         if event.name == 'Hangup' :
             del self.uids [id]
@@ -79,8 +85,24 @@ class Call (object) :
         self.events.append (event)
     # end def append
 
+    def _set_id (self, uniqueid) :
+        self.uniqueid = uniqueid
+        callid = call_id (self.uniqueid)
+        if self.callid :
+            assert (callid == self.callid)
+        self.callid = callid
+        self.manager.register (self)
+    # end def _set_id
+
+    def set_id (self, event) :
+        actionid = event.headers.get ('ActionID')
+        assert (not actionid or actionid == self.actionid)
+        uniqueid = event.headers ['Uniqueid']
+        self._set_id (uniqueid)
+    # end def set_id
+
     def __nonzero__ (self) :
-        return bool (self.uids)
+        return bool (not self.uniqueid or self.uids)
     # end def __nonzero__
 # end class Call
 
@@ -104,63 +126,16 @@ class Call_Manager (object) :
         self.cfg            = cfg = Config ()
         self.manager        = mgr = asterisk.manager.Manager ()
         self.open_calls     = {}
+        self.open_by_id     = {}
+        self.open_by_chan   = {}
         self.closed_calls   = {}
         self.queue          = Queue ()
         self.call_by_number = {}
+        self.unhandled      = []
         mgr.connect (cfg.ASTERISK_HOST)
         mgr.login   (cfg.ASTERISK_MGR_ACCOUNT, cfg.ASTERISK_MGR_PASSWORD)
         mgr.register_event ('*', self.handler)
     # end def __init__
-
-    def handler (self, event, manager) :
-        if 'Uniqueid' in event.headers :
-            self.queue.put (event)
-    # end def handler
-
-    def queue_handler (self, timeout = None) :
-        while self.open_calls :
-            try :
-                event = self.queue.get (timeout = timeout)
-            except Empty :
-                return
-            if 'Uniqueid' in event.headers :
-                uniqueid = event.headers ['Uniqueid']
-                callid   = call_id (uniqueid)
-                if callid in self.open_calls :
-                    call = self.open_calls [callid]
-                    call.append (event)
-                    if not call :
-                        self.closed_calls [callid] = call
-                        del self.open_calls [callid]
-                elif uniqueid != '<null>' :
-                    pass
-                    #print "oops:", callid, event.headers
-            # print "Received event: %s" % event.name
-    # end def queue_handler
-
-    def close (self) :
-        if self.manager :
-            self.manager.close  ()
-            self.manager = None
-    # end def close
-
-    __del__ = close
-
-    def __getattr__ (self, name) :
-        if self.manager and not name.startswith ('__') :
-            result = getattr (self.manager, name)
-            setattr (self, name, result)
-            return result
-        raise AttributeError, name
-    # end def __getattr__
-
-    def originate (self, *args, **kw) :
-        result = self.manager.originate (*args, **kw)
-        #print "Originate:", result.__dict__
-        callid = call_id (result.headers ['Uniqueid'])
-        self.open_calls [callid] = Call (self, callid)
-        return callid
-    # end def originate
 
     def call \
         ( self
@@ -192,7 +167,8 @@ class Call_Manager (object) :
         type    = channel_type   or self.cfg.CHANNEL_TYPE
         suffix  = channel_suffix or self.cfg.CHANNEL_SUFFIX
         callid  = self.originate \
-            ( channel   = '%s/%s%s' % (type, number, suffix)
+            ( self.cfg.MATCH_CHANNEL
+            , channel   = '%s/%s%s' % (type, number, suffix)
             , exten     = call_extension or self.cfg.CALL_EXTENSION
             , context   = call_context   or self.cfg.CALL_CONTEXT
             , priority  = call_priority  or self.cfg.CALL_PRIORITY
@@ -204,6 +180,108 @@ class Call_Manager (object) :
         self.queue_handler (timeout)
         return self.closed_calls [callid]
     # end def call
+
+    def close (self) :
+        if self.manager :
+            self.manager.close  ()
+            self.manager = None
+    # end def close
+
+    def handler (self, event, manager) :
+        if 'Uniqueid' in event.headers :
+            self.queue.put (event)
+    # end def handler
+
+    def originate (self, match_channel = False, *args, **kw) :
+        """ Originate a call with the given asterisk call parameters.
+            If match_channel we try fuzzy-matching on the called channel
+            name when trying to determine the open call. This is needed
+            with asterisk 1.2 since 1.2 doesn't return the unique id.
+        """
+        result = self.manager.originate (*args, **kw)
+        #print "Originate:", result.__dict__
+        actionid = result.headers ['ActionID']
+        uniqueid = result.headers.get ('Uniqueid')
+        #uniqueid = None
+        call = Call (self, actionid, uniqueid)
+        self.open_calls [actionid] = call
+        if match_channel :
+            self.open_by_chan [args ['channel']] = call
+        return actionid
+    # end def originate
+
+    def queue_handler (self, timeout = None) :
+        while self.open_calls :
+            try :
+                event = self.queue.get (timeout = timeout)
+            except Empty :
+                return
+            print "Received event: %s" % event.name, event.headers
+            assert ('Uniqueid' in event.headers)
+            uniqueid = event.headers ['Uniqueid']
+            # ignore bogus uniqueid in asterisk 1.4
+            if uniqueid == '<null>' :
+                continue
+            callid   = call_id (uniqueid)
+            channel  = event.headers.get ('Channel')
+            if channel :
+                channel = '-'.join (channel.split ('-') [:-1])
+            if channel in self.open_by_chan :
+                call = self.open_by_chan [channel]
+                del self.open_by_chan [channel]
+                call.set_id (event)
+            if callid in self.open_by_id :
+                self._handle_queued_event (event)
+            else :
+                self.unhandled.append (event)
+                actionid = event.headers.get ('ActionID')
+                if actionid and actionid in self.open_calls :
+                    call = self.open_calls [actionid]
+                    call.set_id (event)
+                    new_unhandled = []
+                    for e in self.unhandled :
+                        if not self._handle_queued_event (e) :
+                            new_unhandled.append (e)
+                    self.unhandled = new_unhandled
+    # end def queue_handler
+
+    def register (self, call) :
+        """ Called when one of our calls knows its callid. """
+        assert (call.callid not in self.open_by_id)
+        self.open_by_id [call.callid] = call
+    # end def register
+
+    __del__ = close
+
+    def __getattr__ (self, name) :
+        if self.manager and not name.startswith ('__') :
+            result = getattr (self.manager, name)
+            setattr (self, name, result)
+            return result
+        raise AttributeError, name
+    # end def __getattr__
+
+    def _handle_queued_event (self, event) :
+        """ Try handling the event. Returns True on success False
+            otherwise.
+        """
+        print "Handling event: %s" % event.name, event.headers
+        uniqueid = event.headers ['Uniqueid']
+        callid   = call_id (uniqueid)
+        if callid in self.open_by_id :
+            call = self.open_by_id [callid]
+            call.append (event)
+            if not call :
+                actionid = call.actionid
+                self.closed_calls [actionid] = call
+                del self.open_calls [actionid]
+                del self.open_by_id [callid]
+            return True
+        elif uniqueid != '<null>' :
+            pass
+        return False
+    # end def _handle_queued_event
+
 # end class Call_Manager
 
 if __name__ == "__main__" :

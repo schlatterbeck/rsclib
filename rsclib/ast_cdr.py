39c66441efb2d048b26a17b305cb8101194bece5
Ralf Schlatterbeck <rsc@runtux.com>
2009-04-24 10:49:11 +0000

    Add ast_cdr

--- rsclib/ast_cdr.py
+++ rsclib/ast_cdr.py
@@ -2,80 +2,94 @@
 
 import csv
 import time
+from   gzip             import open as gzopen
+from   rsclib.autosuper import autosuper
 
-format = '%Y-%m-%d %H:%M:%S'
+class CDR (autosuper) :
+    def __init__ (self, dictionary) :
+        self.dict = dictionary
+    # end def __init__
 
-class CDR_Parser :
+    def __getattr__ (self, name) :
+        if name in self.dict :
+            value = self.dict [name]
+            setattr (self, name, self.dict [name])
+            return value
+        raise AttributeError, name
+    # end def __getattr__
+
+    def __getitem__ (self, name) :
+        return self.dict [name]
+    # end def __getitem__
+# end class CDR
+
+class CDR_Parser (autosuper) :
+    """ Parse Asterisk CDR records, see CDR_Parser.fields for an
+        explanation of asterisk CDR fields.
+
+        >>> from StringIO import StringIO
+        >>> line = ('"","3","11","attendo","3","lcr/439","IAX2/pbx-14597",'
+        ...         '"Read","dtmf||20|noanswer||3","2009-04-23 15:16:52",'
+        ...         '"2009-04-23 15:16:52","2009-04-23 15:17:37",45,45,'
+        ...         '"ANSWERED","DOCUMENTATION","asterisk-1240499812.774",'
+        ...         '"102441/54"'
+        ...        )
+        >>> p = CDR_Parser (StringIO (line))
+        >>> for cdr in p.iter () :
+        ...     print cdr.amaflags, cdr.disposition, cdr ['channel'], cdr.dst
+        DOCUMENTATION ANSWERED lcr/439 11
+    """
+    fields = \
+        ( ('accountcode', "What account number to use")
+        , ('src'        , "Caller*ID number")
+        , ('dst'        , "Destination extension")
+        , ('dcontext'   , "Destination context")
+        , ('clid'       , "Caller*ID with text")
+        , ('channel'    , "Channel used")
+        , ('dstchannel' , "Destination channel if appropriate")
+        , ('lastapp'    , "Last application if appropriate")
+        , ('lastdata'   , "Last application data (arguments)")
+        , ('start'      , "Start of call (date/time)")
+        , ('answer'     , "Anwer of call (date/time)")
+        , ('end'        , "End of call (date/time)")
+        , ('duration'   , "Total time in system, in seconds (integer), "
+                          "from dial to hangup")
+        , ('billsec'    , "Total time call is up, in seconds (integer), "
+                          "from answer to hangup")
+        , ('disposition', "What happened to the call: "
+                          "ANSWERED, NO ANSWER, BUSY, FAILED")
+        , ('amaflags'   , "DOCUMENTATION, BILLING, IGNORE etc, "
+                          "specified on a per channel ")
+        , ('userfield'  , "A user-defined field, maximum 255 characters")
+        , ('uniqueid'   , "Unique Channel Identifier")
+        )
+    
     def __init__ (self, * files) :
         self.files = files
     # end def __init__
 
-    def time_ok (self, t) :
-        date = time.strftime ('%Y-%m-%d ', t)
-        time_to   = time.strptime (date + self.time_to,   '%Y-%m-%d %H:%M')[3:6]
-        time_from = time.strptime (date + self.time_from, '%Y-%m-%d %H:%M')[3:6]
-        t         = t [3:6]
-        if not time_from or not time_to :
-            return True
-        if time_from < time_to :
-            if not time_from <= t < time_to :
-                return False
-        else :
-            if time_to < t <= time_from :
-                return False
-        return True
-    # end def time_ok
-
-    def sum_over \
-        ( self, callee_prefix
-        , date_from = None
-        , date_to   = None
-        , time_from = None
-        , time_to   = None
-        ) :
-        self.time_from = time_from
-        self.time_to   = time_to
-        if time_from and not time_to or time_to and not time_from :
-            raise ValueError, "Specify both or none of time_from, time_to"
-        sum = 0
+    def iter (self) :
+        """ Iterator works only once if self.files are file objects """
         for f in self.files :
-            reader = csv.reader (open (f), dialect = 'excel', delimiter = ',')
-            for line in reader :
-                if line [14] != 'ANSWERED' :
-                    continue
-                start = time.strptime (line [10], format)
-                end   = time.strptime (line [11], format)
-                try :
-                    call = line[8].split (':')[1][1:]
-                except IndexError :
-                    call = ""
-                if (   call.startswith (callee_prefix)
-                   and (not date_from or date_from < start)
-                   and (not date_to   or start     < date_to)
-                   and (not time_from or self.time_ok (start))
-                   and (not time_to   or self.time_ok (start))
-                   ) :
-                    sum += time.mktime (end) - time.mktime (start)
-        return sum
-    # end def sum_over
+            if hasattr (f, 'read') :
+                fd = f
+            else :
+                if f.endswith ('.gz') :
+                    fd = gzopen (f, 'r')
+                else :
+                    fd = open (fn, 'r')
+            reader = csv.DictReader \
+                ( fd
+                , fieldnames = [f [0] for f in self.fields]
+                , dialect    = 'excel'
+                , delimiter  = ','
+                )
+            for record in reader :
+                yield (CDR (record))
+            fd.close ()
+    # end def iter
+
 # end class CDR_Parser
 
 if __name__ == '__main__' :
-    import sys
-    p = CDR_Parser (* sys.argv [4:])
-    f = format.split (' ')[0]
-    if sys.argv [1] == 'F' :
-        time_from = '18:00'
-        time_to   = '08:00'
-    elif sys.argv [1] == 'G' :
-        time_from = '08:00'
-        time_to   = '18:00'
-    else :
-        time_from = time_to = None
-    print p.sum_over \
-        ( '1003'
-        , date_from = time.strptime (sys.argv [2], f)
-        , date_to   = time.strptime (sys.argv [3], f)
-        , time_from = time_from
-        , time_to   = time_to
-        )
+    pass

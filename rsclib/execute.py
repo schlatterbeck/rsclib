34af1a04627b224fa0f89ce3d02e74e86132abe7
Ralf Schlatterbeck <rsc@runtux.com>
2009-07-22 06:00:14 +0000

    Simple pipe with internal processes
    *and* execed subprograms works now

--- rsclib/execute.py
+++ rsclib/execute.py
@@ -24,12 +24,14 @@ import os
 import sys
 import logging
 import errno
+import signal
 from   logging.handlers import SysLogHandler
 from   traceback        import format_exc
 from   subprocess       import Popen, PIPE
 from   rsclib.autosuper import autosuper
 
 class Exec_Error    (RuntimeError) : pass
+class Dead_Client   (RuntimeError) : pass
 
 class _Named (autosuper) :
     def _name (self) :
@@ -130,3 +132,208 @@ class Lock_Mixin (_Named) :
             os.unlink (self.lockfile)
     # end def __del__
 # end class Lock_Mixin
+
+def exitstatus (cmd, status) :
+    if not status :
+        return ''
+    sig = ''
+    if os.WIFSIGNALED (status) :
+        sig = " (caught signal %d)" % os.WTERMSIG (status)
+    s = os.WEXITSTATUS (status)
+    if not s : s = ''
+    return 'WARNING: %s returned nonzero exit status %s%s' % (cmd, s, sig)
+# end def exitstatus
+
+class Process (_Named) :
+    """ A process in a (multi-)pipe
+    """
+
+    by_pid = {}
+
+    def __init__ \
+        (self
+        , name   = None
+        , stdin  = None
+        , stdout = None
+        , stderr = None
+        , **kw
+        ) :
+        self.__super.__init__ (**kw)
+        self.stdin     = stdin
+        self.stdout    = stdout
+        self.stderr    = stderr
+        self.status    = None
+        self.name      = name
+        if not self.name :
+            self.name  = self.clsname
+        self.children  = []
+        self.tee       = None
+        self.pid       = None
+        self.toclose   = []
+    # end def __init__
+
+    def fork (self) :
+        pid = os.fork ()
+        if pid : # parent
+            self.pid = pid
+            self.by_pid [pid] = self
+            if self.stdout :
+                self.stdout.close ()
+            if self.stderr :
+                self.stderr.close ()
+            if self.stdin :
+                self.stdin.close ()
+        else : # child
+            for f in self.toclose :
+                f.close ()
+            self.method ()
+            sys.exit    (0)
+        return pid
+    # end def fork
+
+    def method (self) :
+        raise NotImplementedError
+    # end def method
+
+    def run (self) :
+        return self.fork ()
+    # end def run
+
+    @classmethod
+    def wait (cls) :
+        while cls.by_pid :
+            print cls.by_pid.keys ()
+            pid, status = os.wait ()
+            if pid > 0 :
+                cls.by_pid [pid].status = status
+            del cls.by_pid [pid]
+    # end def wait
+# end class Process
+
+class Tee (Process) :
+    """ A tee in a pipe (like the unix command tee but copies to several
+        sub-processes)
+    """
+    def __init__ (self, children, bufsize = 4094, **kw) :
+        self.stdouts  = {}
+        self.bufsize  = bufsize
+        self.children = children
+        for c in self.children :
+            pipe = os.pipe ()
+            print "tee pipe:", pipe
+            self.stdouts [os.fdopen (pipe [1], 'w')] = True
+            c.stdin = os.fdopen (pipe [0], 'r')
+        self.__super.__init__ (**kw)
+    # end def __init__
+
+    def method (self) :
+        print self.name, "method"
+        osig = signal.signal (signal.SIGPIPE, self.sig_pipe)
+        print self.name, "after signal"
+        while 1 :
+            print self.name, "before read", self.stdin
+            buf = self.stdin.read (self.bufsize)
+            print "read:", len (buf)
+            if not buf :
+                print "Tee: empty read, terminating"
+                return
+            # use items () here, we want to modify dict
+            written = False
+            for stdout, state in self.stdouts.items () :
+                if not state :
+                    continue
+                try :
+                    stdout.write (buf)
+                    written = True
+                    print "written:", len (buf)
+                except Dead_Client :
+                    # this client died, no longer try to send to it
+                    print "Dead:", stdout
+                    self.stdouts [stdout] = False
+            # still clients existing?
+            if not written :
+                return
+    # end def method
+
+    def sig_pipe (self, sig, frame) :
+        raise Dead_Client, "received SIGPIPE"
+    # end def sig_pipe
+
+# end class Tee
+
+class Method_Process (Process) :
+    """ A process in a (multi-)pipe which runs a given method
+    """
+    def __init__ (self, name = None, **kw) :
+        if 'method' in kw :
+            self._method = kw ['method']
+        self.name      = name
+        if not self.name :
+            self.name  = self._method.__name__
+        self.__super.__init__ (name = self.name, **kw)
+    # end def __init__
+
+    def append (self, child) :
+        assert (not self.stdout)
+        assert (not child.stdin)
+        self.children.append (child)
+    # end def append
+
+    def method (self) :
+        self.redirect ()
+        self._method ()
+    # end def method
+
+    def redirect (self) :
+        if self.stdin :
+            os.dup2 (self.stdin.fileno (), sys.stdin.fileno ())
+            self.stdin.close ()
+        if self.stdout :
+            os.dup2 (self.stdout.fileno (), sys.stdout.fileno ())
+            self.stdout.close ()
+        if self.stderr :
+            os.dup2 (self.stderr.fileno (), sys.stderr.fileno ())
+            self.stderr.close ()
+    # end def redirect
+
+    def run (self) :
+        if self.children :
+            pipe  = os.pipe ()
+            stdin = os.fdopen (pipe [0], 'r')
+            self.stdout = os.fdopen (pipe [1], 'w')
+            if len (self.children) > 1 :
+                self.tee = Tee (self.children, stdin = stdin)
+                self.tee.toclose.append (self.stdout)
+                print "Tee stdin:", stdin.fileno ()
+            elif len (self.children) :
+                child = self.children [0]
+                child.stdin = stdin
+                child.toclose.append (self.stdout)
+        for child in self.children :
+            child.run ()
+        if self.tee :
+            pid = self.tee.run ()
+            self.by_pid [pid] = self.tee
+        return self.fork ()
+    # end def run
+
+# end class Method_Process
+
+class Exec_Process (Method_Process) :
+    """ Process that execs a subcommand """
+    def __init__ (self, cmd, args = None, name = None, **kw) :
+        self.cmd  = cmd
+        self.args = args
+        if not args :
+            self.args = [cmd]
+        if not name :
+            name = self.cmd
+        self.__super.__init__ (name = name, **kw)
+    # end def __init__
+
+    def method (self) :
+        self.redirect ()
+        os.execv (self.cmd, self.args)
+    # end def method
+
+# end class Exec_Process

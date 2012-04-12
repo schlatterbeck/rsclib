48e947b1e439891ac0455cead22c36b4bb6ee80a
Ralf Schlatterbeck <rsc@runtux.com>
2012-04-12 12:47:34 +0000

    Fix copyright date

--- rsclib/ast_call.py
+++ rsclib/ast_call.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (C) 2008-11 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Copyright (C) 2008-12 Dr. Ralf Schlatterbeck Open Source Consulting.
 # Reichergasse 131, A-3411 Weidling.
 # Web: http://www.runtux.com Email: office@runtux.com
 # All rights reserved

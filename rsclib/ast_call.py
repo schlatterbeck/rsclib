1c82d6ac9087bbf867547e2303be3c8568178424
Ralf Schlatterbeck <rsc@runtux.com>
2020-01-14 15:04:00 +0100

    In python3.7 async is a reserved word
    .. and pyst agi now uses asynchronous as the parameter name.

--- rsclib/ast_call.py
+++ rsclib/ast_call.py
@@ -661,14 +661,14 @@ class Call_Manager (object) :
             account = self.cfg.ACCOUNT_CODE
         actionid  = self.originate \
             ( self.cfg.MATCH_CHANNEL
-            , channel   = '%s/%s%s' % (type, number, suffix)
-            , exten     = call_extension or self.cfg.CALL_EXTENSION
-            , context   = call_context   or self.cfg.CALL_CONTEXT
-            , priority  = call_priority  or self.cfg.CALL_PRIORITY
-            , caller_id = caller_id      or self.cfg.CALLER_ID
-            , account   = account
-            , async     = True
-            , variables = vars
+            , channel      = '%s/%s%s' % (type, number, suffix)
+            , exten        = call_extension or self.cfg.CALL_EXTENSION
+            , context      = call_context   or self.cfg.CALL_CONTEXT
+            , priority     = call_priority  or self.cfg.CALL_PRIORITY
+            , caller_id    = caller_id      or self.cfg.CALLER_ID
+            , account      = account
+            , asynchronous = True
+            , variables    = vars
             )
         self.call_by_number [actionid] = number
         # maybe call already terminated or we have a permission problem:

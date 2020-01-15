31e7a21b0861506bfa9b0cb27b2b63927ef55f01
Ralf Schlatterbeck <rsc@runtux.com>
2020-01-15 13:53:19 +0100

    Add unhexdump

--- rsclib/hexdump.py
+++ rsclib/hexdump.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (C) 2011-13 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Copyright (C) 2011-19 Dr. Ralf Schlatterbeck Open Source Consulting.
 # Reichergasse 131, A-3411 Weidling.
 # Web: http://www.runtux.com Email: office@runtux.com
 # All rights reserved
@@ -18,6 +18,8 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 # ****************************************************************************
+
+import sys
 
 def ascii (s) :
     if len (repr (s)) > 3 :
@@ -38,3 +40,34 @@ def hexdump (s, start = 0, show_addr = True) :
     return '\n'.join (r)
 # end def hexdump
 
+def unhexdump (iterable) :
+    """ Convert a hex-dump to binary
+    """
+    bin = []
+    end = False
+    for line in iterable :
+        line = line.strip ()
+        if not line :
+            continue
+        if end :
+            raise ValueError ("Unknown hexdump format")
+        x = line.split ('  ', 2)
+        if len (x) <= 2 :
+            h = x [0]
+        else :
+            h = x [1]
+            assert len (x [0]) <= 8
+        # Probably last line with only an address
+        if ' ' not in h and len (x) == 1 and len (h) > 2 :
+            end = True
+            continue
+        h = h.strip ().split (' ')
+        if len (h [0]) > 2 :
+            h = h [1:]
+        if sys.version_info [0] < 3 :
+            method = lambda x : b''.join (chr (c) for c in x)
+        else :
+            method = bytes
+        bin.append (method ((int (k, 16)) for k in h))
+    return b''.join (bin)
+# end def unhexdump
